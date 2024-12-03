89a0376693003197468b241619eb4b66e2646621
Alex Blackmore <ablackmore113@gmail.com>
2024-12-03 17:39:18 +1100

    golf

--- 03/sol.py
+++ 03/sol.py
@@ -4,12 +4,5 @@ with open("input.txt") as file:
     line = file.read().strip()
     mul = lambda x, y: x * y
     print(sum([eval(x) for x in re.findall(r'mul\(\d+,\d+\)', line)]))
-    on = True
-    total = 0
-    instrs = re.findall(r"(mul\(\d+,\d+\))|(do\(\))|(don't\(\))", line)
-    for inst in instrs:
-        if inst[0] and on:
-            total += eval(inst[0])
-        elif not inst[0]:
-            on = inst[1]
-    print(total)
+    valid = line.split('don\'t()')[0] + ''.join([''.join(x.split('do()')[1:]) for x in line.split('don\'t()')[1:]])
+    print(sum([eval(x) for x in re.findall(r'mul\(\d+,\d+\)', valid)]))
