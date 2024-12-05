23b4fef4605ba399681584c47de9d00bbf4eebf0
Alex Blackmore <ablackmore113@gmail.com>
2024-12-05 16:40:27 +1100

    remove unneeded

--- 05/sol.py
+++ 05/sol.py
@@ -21,4 +21,4 @@ with open("input.txt") as file:
     updates = [arr.split(",") for arr in s2.strip().split("\n")]
     print(sum([int(arr[(len(arr) - 1) // 2]) for arr in updates if good(rules, arr)]))
     mod = [reorder(rules, line) for line in updates if reorder(rules, line)]
-    print(sum([int(arr[(len(arr) - 1) // 2]) for arr in mod if good(rules, arr)]))
+    print(sum([int(arr[(len(arr) - 1) // 2]) for arr in mod]))
